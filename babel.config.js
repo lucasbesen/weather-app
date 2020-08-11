75c97d5bb0f7b63799784420887ac7514724f45c
lucasbesen <lucassilvabesen@gmail.com>
2020-08-11 19:05:48 -0300

    chore(app): setup module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['@babel/plugin-proposal-optional-chaining'],
+    plugins: [
+      '@babel/plugin-proposal-optional-chaining',
+      [
+        'module-resolver',
+        {
+          alias: {
+            app: './app',
+          },
+        },
+      ],
+    ],
   };
 };
